9b7789eb546b21c6dda7c5730d1f7b42e0fadf52
EmanuelaValenti <emanuela.v94@hotmail.com>
2020-05-21 10:03:46 +0200

    Initial RMI commit

--- src/PersonList.java
+++ src/PersonList.java
@@ -1,21 +1,21 @@
+import java.awt.image.AreaAveragingScaleFilter;
 import java.util.ArrayList;
 
 public class PersonList {
     private ArrayList<Person> list = new ArrayList<>();
 
-    //Potrebbe tornare un booleano se voglio sapere se una persona è stata aggiunta
-    public void addPerson(Person p){
+    public void addPerson(Person p) {
         list.add(p);
     }
 
-    public ArrayList<Person> getList(){
+    public ArrayList<Person> getList() {
         //return list;
         ArrayList<Person> anotherlist = new ArrayList<>();
-        for(Person p){
-            //costruisco una nuova persona (buono per oggetti immutabili)
+        for (Person p:list) {
             Person x = new Person(p.getName(),p.getAge());
             anotherlist.add(x);
         }
+
         return anotherlist;
     }
 }
